d910b41f6a399a14d10eca5bcaa04b131504f23b
Josiah Webb <jj.webb520@gmail.com>
2022-11-20 17:10:35 -0700

    adds notes to fragment shader file, creates random values and sets values as attribute on geometry

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -1,3 +1,5 @@
+// precision comes in high, low, and medium 'p'
+// low is rarely used due to bugs it can cause, medium is most used, high is best but least performant (doesn't work on all devices)
 precision mediump float;
 
 void main() {
