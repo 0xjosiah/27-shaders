4a2b36728bc53f1401e382eb67c53a6a48203e2f
Josiah Webb <jj.webb520@gmail.com>
2022-11-21 15:41:17 -0700

    uses uniform uColor attribute to color fragment shader

--- src/script.js
+++ src/script.js
@@ -48,7 +48,8 @@ const material = new THREE.RawShaderMaterial({
     // transparent: true,
     uniforms: {
         uFrequency: { value: new THREE.Vector2(10, 5) },
-        uTime: { value: 0, }
+        uTime: { value: 0, },
+        uColor: { value: new THREE.Color(0xf9009f) }
     }
 })
 
--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -1,9 +1,11 @@
 // precision comes in high, low, and medium 'p'
 // low is rarely used due to bugs it can cause, medium is most used, high is best but least performant (doesn't work on all devices)
 precision mediump float;
+uniform vec3 uColor;
+
 
 varying float vRandom;
 
 void main() {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    gl_FragColor = vec4(uColor, 1.0);
 }
